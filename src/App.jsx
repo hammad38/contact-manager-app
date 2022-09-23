56c634e344713f0f51e853eb290c95d7c1d35241
hammad38 <hammadsami119@gmail.com>
2022-09-23 16:56:21 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import AddContact from './AddContact'
+import ContactList from './ContactList'
 import Header from './Header'
 const App = () => {
   return (
     <div>
       <Header />
+      <AddContact />
+      <ContactList />
     </div>
   )
 }
