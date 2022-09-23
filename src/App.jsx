a2d4f89483172e7bd4e02927a82af275b0318130
hammad38 <hammadsami119@gmail.com>
2022-09-23 17:06:57 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ const App = () => {
   return (
     <div>
       <Header />
-      <AddContact />
-      <ContactList />
+      {/* <AddContact />
+      <ContactList /> */}
     </div>
   )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <div>
-      <div className="main ui">Contact</div>
+      <div className="ui fixed menu">
+        <div className="ui container center">
+          <h2>Contact Manager</h2>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
